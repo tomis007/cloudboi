facc60783316099935cf390b951fd2f4e0053d0a
tomis007 <thomas.hendrickson@tufts.edu>
2017-07-10 22:27:44 -0400

    updated build option

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # quit on mvn failure, TODO better way to handle?
 set -e # script exits on any command failure
 # Reset in case getopts has been used
@@ -8,57 +7,56 @@ OPTIND=1
 maven=false
 
 function show_help() {
-        echo "Usage: ./deploy.sh -{h?m}"
+        echo "Usage: ./deploy.sh -{h?b}"
         echo "       -h, -?   show this message"
-        echo "       -m       clean,compile,package the maven project"
+        echo "       -b       clean,compile,package the maven project"
         echo "                before deploying to tomcat"
 }
 
-while getopts "mh?" opt; do
+while getopts "bh?" opt; do
         case $opt in
         h|\?)
                 show_help
                 exit 0
                 ;;
-        m)
+        b)
                 maven=true
                 ;;
         esac
 done
 
-# check for pom
+# maven pom
 if [ ! -f $(pwd)/pom.xml ]; then
         echo "No pom.xml found"
         exit 1
 fi
 
-# mvn clean, compile, package TODO? All necessary?
+# TODO? All necessary?
 if [ "$maven" == true ]; then
         mvn clean
         mvn compile
         mvn package
 fi
 
-# Check for CATALINA_HOME
+# tomcat's CATALINA_HOME
 if [ ! -z "${CATALINA_HOME:?Need to set CATALINA_HOME}" ]; then
         echo "INFO: Starting deploy to tomcat"
 fi
 
-# copy war to tomcat, clean up old files
+# deploy to tomcat
 if [ -f $(pwd)/target/CloudBoi.war ]; then
-        # delete files at tomcat home
+        # delete old files
         if [ -f $CATALINA_HOME/webapps/CloudBoi.war ]; then
                 rm $CATALINA_HOME/webapps/CloudBoi.war
         fi
         if [ -d $CATALINA_HOME/webapps/CloudBoi ]; then
                 rm -r $CATALINA_HOME/webapps/CloudBoi
         fi
-        # copy war
+        # deploy
         cp $(pwd)/target/CloudBoi.war $CATALINA_HOME/webapps/
-        echo "SUCCESS: Deployed war"
+        echo "SUCCESS: Deployed war to tomcat"
         exit 0
 else
-        # couldn't find war
         echo "ERROR: CloudBoi.war not found, could not deploy"
         exit 1
 fi
